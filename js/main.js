273a0f2ec116ba043afdc5829f0ec5f5267d97f2
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-09 16:14:17 +0100

    FORMATTATO TUTTI I MESI in numero mese con moment.js

--- js/main.js
+++ js/main.js
@@ -12,17 +12,35 @@ $(document).ready(function() {
           labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno","Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
           datasets: [{
             label: "Vendite",
-            data: [1230, 2000, 500, 2800, 3500, 1000, 3980,1230, 2000, 500, 2800, 3500, 1000]
+            data: [1230, 2000, 500, 2800, 3500, 1000, 3980,1230, 2000, 500, 2800, 3500, 1000],
+            lineTension: 0,
+            backgroundColor: 'transparent',
           }]
       }
 
   });
+
+  /*var mese = moment("27-11-1984","DD-MM-YYY");
+  console.log(mese.format("M"));
+  $("h1").append(mese.format("M"));*/
+
   //invoco una chiamata ajax in get per leggere tutti i dati
   $.ajax({
     url: url,
     type: "GET",
     success: function(data){
       console.log(data);
+
+      var totaleGennaio = 0 ;
+
+      for (var i = 0; i < data.length; i++) {
+        /*FORMATTATO TUTTE LE DATE IN NUMERO MESE*/
+        var dataGenerale = data[i].date;
+        var mese = moment(dataGenerale,"DD/MM/YYY");
+        var meseFormattato = mese.format("M");
+        console.log(meseFormattato);
+        
+      }
     },
     error: function(errore){
       console.log(errore);
