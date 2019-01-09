4f4329fc893e9e3e5fd56ad8fcbee4089fe22c53
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-09 15:30:05 +0100

    realizzato il grafico con dati statici

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   //alert("vivo");
   //variabilizzo la mia uri
   var url = "http://157.230.17.132:4006/sales"
-
+  //metto un target su html
   var graficoVenditeCanvas = $("#myChart");
 
+  //creo il grafico
   var graficoVenditeMensili = new Chart(graficoVenditeCanvas, {
       type: 'line',
       data: {
           labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno","Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
           datasets: [{
-
+            label: "Vendite",
+            data: [1230, 2000, 500, 2800, 3500, 1000, 3980,1230, 2000, 500, 2800, 3500, 1000]
           }]
       }
 
