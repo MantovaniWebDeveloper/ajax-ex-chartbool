ba71f9a2df7289ebb2a31f7fdcf52fea1438447d
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-09 15:20:49 +0100

    inizio grafico vendite

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
   //variabilizzo la mia uri
   var url = "http://157.230.17.132:4006/sales"
 
+  var graficoVenditeCanvas = $("#myChart");
+
+  var graficoVenditeMensili = new Chart(graficoVenditeCanvas, {
+      type: 'line',
+      data: {
+          labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno","Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+          datasets: [{
+
+          }]
+      }
+
+  });
   //invoco una chiamata ajax in get per leggere tutti i dati
   $.ajax({
     url: url,
