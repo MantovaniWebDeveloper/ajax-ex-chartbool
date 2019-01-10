a4d8f0b49f90e020ed6f487dbfbc169205230575
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-10 10:09:06 +0100

    creato grafica torta con dati di esempio statici

--- js/main.js
+++ js/main.js
@@ -9,122 +9,171 @@ $(document).ready(function() {
   $("h1").append(mese.format("M"));*/
 
   //invoco una chiamata ajax in get per leggere tutti i dati
+  //e sommare le vendite per ogni mese
   $.ajax({
     url: url,
     type: "GET",
     success: function(data) {
       console.log(data);
+      calcoloFatturatoMensile(data);
+      calcoloPercentualeVenditore(data);
 
-      var totaleGennaio = 0;
-      var totaleFebbraio = 0;
-      var totaleMarzo = 0;
-      var totaleAprile = 0;
-      var totaleMaggio = 0;
-      var totaleGiugno = 0;
-      var totaleLuglio = 0;
-      var totaleAgosto = 0;
-      var totaleSettembre = 0;
-      var totaleOttobre = 0;
-      var totaleNovembre = 0;
-      var totaleDicembre = 0;
-
-
-
-      for (var i = 0; i < data.length; i++) {
-        console.log("id " + data[i].id);
-        /*FORMATTATO TUTTE LE DATE IN NUMERO MESE*/
-        var dataGenerale = data[i].date;
-        var mese = moment(dataGenerale, "DD/MM/YYY");
-        var meseFormattato = mese.format("M");
-        console.log("mese " + meseFormattato);
-        var vendita = data[i].amount;
-        console.log("vendita " + vendita);
-
-        switch (meseFormattato) {
-
-          case "1":
-            totaleGennaio = totaleGennaio + vendita;
-            break;
-
-          case "2":
-            totaleFebbraio = totaleFebbraio + vendita;
-            break;
-
-          case "3":
-            totaleMarzo = totaleMarzo + vendita;
-            break;
-
-          case "4":
-            totaleAprile = totaleAprile + vendita;
-            break;
-
-          case "5":
-            totaleMaggio = totaleMaggio + vendita;
-            break;
-
-          case "6":
-            totaleGiugno = totaleGiugno + vendita;
-            break;
-
-          case "7":
-            totaleLuglio = totaleLuglio + vendita;
-            break;
-
-          case "8":
-            totaleAgosto = totaleAgosto + vendita;
-            break;
-
-          case "9":
-            totaleSettembre = totaleSettembre + vendita;
-            break;
-
-          case "10":
-            totaleOttobre = totaleOttobre + vendita;
-            break;
-
-          case "11":
-            totaleNovembre = totaleNovembre + vendita;
-            break;
-
-          case "12":
-            totaleDicembre = totaleDicembre + vendita;
-            break;
-        }
-      }
-      console.log("totale gennaio " + totaleGennaio);
-      console.log("totale febbraio " + totaleFebbraio);
-      console.log("totale marzo " + totaleMarzo);
-      console.log("totale aprile " + totaleAprile);
-      console.log("totale maggio " + totaleMaggio);
-      console.log("totale giugno " + totaleGiugno);
-      console.log("totale Luglio " + totaleLuglio);
-      console.log("totale Agosto " + totaleAgosto);
-      console.log("totale Settembre " + totaleSettembre);
-      console.log("totale ottobre " + totaleOttobre);
-      console.log("totale Novembre " + totaleNovembre);
-      console.log("totale dicembre " + totaleDicembre);
-
-      //metto un target su html
-      var graficoVenditeCanvas = $("#myChart");
-
-      //creo il grafico
-      var graficoVenditeMensili = new Chart(graficoVenditeCanvas, {
-        type: 'line',
-        data: {
-          labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
-          datasets: [{
-            label: "Vendite",
-            data: [totaleGennaio, totaleFebbraio, totaleMarzo, totaleAprile, totaleMaggio, totaleGiugno, totaleLuglio, totaleAgosto, totaleSettembre, totaleOttobre, totaleNovembre, totaleDicembre],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-          }]
-        }
-
-      });
     },
     error: function(errore) {
       console.log(errore);
     }
   });
+
+
+
+  function calcoloFatturatoMensile (data){
+    var totaleGennaio = 0;
+    var totaleFebbraio = 0;
+    var totaleMarzo = 0;
+    var totaleAprile = 0;
+    var totaleMaggio = 0;
+    var totaleGiugno = 0;
+    var totaleLuglio = 0;
+    var totaleAgosto = 0;
+    var totaleSettembre = 0;
+    var totaleOttobre = 0;
+    var totaleNovembre = 0;
+    var totaleDicembre = 0;
+
+
+    for (var i = 0; i < data.length; i++) {
+      console.log("id " + data[i].id);
+      /*FORMATTATO TUTTE LE DATE IN NUMERO MESE*/
+      var dataGenerale = data[i].date;
+      var mese = moment(dataGenerale, "DD/MM/YYY");
+      var meseFormattato = mese.format("M");
+      console.log("mese " + meseFormattato);
+      /*VENDITE*/
+      var vendita = data[i].amount;
+      console.log("vendita " + vendita);
+      /*NOMI VENDITORI*/
+      var nome = data[i].salesman;
+      console.log("nome venditore " + nome);
+
+      switch (meseFormattato) {
+
+        case "1":
+          totaleGennaio = totaleGennaio + vendita;
+          break;
+
+        case "2":
+          totaleFebbraio = totaleFebbraio + vendita;
+          break;
+
+        case "3":
+          totaleMarzo = totaleMarzo + vendita;
+          break;
+
+        case "4":
+          totaleAprile = totaleAprile + vendita;
+          break;
+
+        case "5":
+          totaleMaggio = totaleMaggio + vendita;
+          break;
+
+        case "6":
+          totaleGiugno = totaleGiugno + vendita;
+          break;
+
+        case "7":
+          totaleLuglio = totaleLuglio + vendita;
+          break;
+
+        case "8":
+          totaleAgosto = totaleAgosto + vendita;
+          break;
+
+        case "9":
+          totaleSettembre = totaleSettembre + vendita;
+          break;
+
+        case "10":
+          totaleOttobre = totaleOttobre + vendita;
+          break;
+
+        case "11":
+          totaleNovembre = totaleNovembre + vendita;
+          break;
+
+        case "12":
+          totaleDicembre = totaleDicembre + vendita;
+          break;
+      };
+    }
+    console.log("totale gennaio " + totaleGennaio);
+    console.log("totale febbraio " + totaleFebbraio);
+    console.log("totale marzo " + totaleMarzo);
+    console.log("totale aprile " + totaleAprile);
+    console.log("totale maggio " + totaleMaggio);
+    console.log("totale giugno " + totaleGiugno);
+    console.log("totale Luglio " + totaleLuglio);
+    console.log("totale Agosto " + totaleAgosto);
+    console.log("totale Settembre " + totaleSettembre);
+    console.log("totale ottobre " + totaleOttobre);
+    console.log("totale Novembre " + totaleNovembre);
+    console.log("totale dicembre " + totaleDicembre);
+
+    //metto un target su html
+    var graficoVenditeCanvas = $("#myChart");
+
+    //creo il grafico
+    var graficoVenditeMensili = new Chart(graficoVenditeCanvas, {
+      type: 'line',
+      data: {
+        labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+        datasets: [{
+          label: "Vendite",
+          data: [totaleGennaio, totaleFebbraio, totaleMarzo, totaleAprile, totaleMaggio, totaleGiugno, totaleLuglio, totaleAgosto, totaleSettembre, totaleOttobre, totaleNovembre, totaleDicembre],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: 'blue',
+        }]
+      }
+
+    });
+  }
+  function calcoloPercentualeVenditore(data) {
+
+    var venditoreObj = {};
+
+    for (var i = 0; i < data.length; i++) {
+      console.log("id " + data[i].id);
+      /*VENDITE*/
+      var vendita = data[i].amount;
+      console.log("vendita " + vendita);
+      /*NOMI VENDITORI*/
+      var nome = data[i].salesman;
+      console.log("nome venditore " + nome);
+      venditoreObj.nomeVenditore = nome;
+      venditoreObj.venduto = vendita;
+    }
+
+    //metto un target su html
+    var graficoPercentualeVendite = $("#myChartTorta");
+
+    //creo il grafico
+    var graficoVenditeMensili = new Chart(graficoPercentualeVendite, {
+      type: 'doughnut',
+      data : {
+        labels: ["Dario","Andrea","Cosimo","Lorenzo"],
+        datasets: [{
+          data: [24,50,47,24],
+          backgroundColor: [
+              "#FF6384",
+              "#63FF84",
+              "#84FF63",
+              "#8463FF"
+          ]
+        }]
+      }
+
+    });
+  }
 
 });
